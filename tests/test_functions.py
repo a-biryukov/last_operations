352b5eeeb4e77e98730d68ad5a5ded8e660e415c
Alexander <a.biryukov164@gmail.com>
2024-02-01 09:40:41 +0400

    change name func

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,4 @@
-from src.utils.functions import load_file, get_last_operations, enter_operations
+from src.utils.functions import load_file, get_last_operations, get_operations_information
 
 
 def test_load_file(filename_fixture):
@@ -9,7 +9,7 @@ def test_get_last_operations(data_fixture, last_operations_fixture):
     assert get_last_operations(data_fixture) == last_operations_fixture
 
 
-def test_enter_operations(last_operations_fixture, enter_fixture_1, enter_fixture_2, enter_fixture_3):
-    assert enter_operations(last_operations_fixture[0]) == enter_fixture_1
-    assert enter_operations(last_operations_fixture[1]) == enter_fixture_2
-    assert enter_operations(last_operations_fixture[2]) == enter_fixture_3
+def test_get_information_operations(last_operations_fixture, info_fixture_1, info_fixture_2, info_fixture_3):
+    assert get_operations_information(last_operations_fixture[0]) == info_fixture_1
+    assert get_operations_information(last_operations_fixture[1]) == info_fixture_2
+    assert get_operations_information(last_operations_fixture[2]) == info_fixture_3
